6ba0a74f6881a7ec5888f6b6c227940f4d14a9b1
laofang <429856304@qq.com>
2019-11-09 23:36:20 +0800

    lambda相关接口 测试Function接口

--- src/main/java/com/galaxy/venus/lambda/LambdaTest.java
+++ src/main/java/com/galaxy/venus/lambda/LambdaTest.java
@@ -1,15 +1,22 @@
 package com.galaxy.venus.lambda;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class LambdaTest {
 
     public static void main(String[] args) {
         add(p-> System.out.println(p + 10));
+        System.out.println(modify(x-> x + 10 + ""));
     }
 
 
     public static void add(Consumer<Integer> consumer) {
         consumer.accept(10);
+    }
+
+
+    public static String modify(Function<Integer,String> function) {
+        return function.apply(10);
     }
 }
