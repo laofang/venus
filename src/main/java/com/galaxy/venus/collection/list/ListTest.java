1ea6230924dad5337cb8f57e113a054737a7a722
laofang <429856304@qq.com>
2019-11-11 23:48:24 +0800

    stream流将List优雅转换成指定前缀，分割符，后缀的字符串

---
+++ src/main/java/com/galaxy/venus/collection/list/ListTest.java
@@ -0,0 +1,14 @@
+package com.galaxy.venus.collection.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<String> studentList = new ArrayList<>(Arrays.asList(new String[]{"123","234","345"}));
+        String tempStr = studentList.parallelStream().collect(Collectors.joining("|","[","]"));
+        System.out.println(tempStr);
+    }
+}
